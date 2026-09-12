e404b46899acdde87b1a21f4f03a0290299603db
DECStudioHUB <dantecustodio13@gmail.com>
2026-09-13 00:13:08 +0800

    Refactor vite.config.ts for cleaner configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
       },
     },
     server: {
